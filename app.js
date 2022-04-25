2e57ee828670680145042cca0ae4db2993f32e3f
PrAkHaR-BitsPilani <56273982+PrAkHaR-BitsPilani@users.noreply.github.com>
2022-04-25 21:11:58 +0530

    kuch kuch kiya

--- app.js
+++ app.js
@@ -6,56 +6,94 @@ const bodyParser = require('body-parser')
 
 const app = express()
 app.use(bodyParser.urlencoded({extended : true}))
+app.use(express.static('public'))
+app.set('view engine' , 'ejs')
 
 const { blockchain } = require('./src/blockchain')
-const { User } = require('./src/user')
+const { User , createUser} = require('./src/user')
 
 const URL = 'mongodb://localhost:27017'
-const dbName = 'BITS-Voting'
+const dbName = 'crypto'
 const PORT = 3000
 
-mongoose.connect(URL + '/' + dbName)
+// mongoose.connect(URL + '/' + dbName)
+
 const candidateSchema = new mongoose.Schema({
     name: String,
     phone_no : Number,
-    description: String,
     loc : String
 })
 const Candidate = mongoose.model('Candidate' , candidateSchema)
 
-const userSchema = new mongoose.Schema({
-    user_obj : Object
-})
 
-
-let somuCoin = new blockchain()
-
-
-voters = [
-    new User('7651886038'), // prakhar
-    new User('8081452254'), //vedansh
-    new User('9305073399'), //jeevan
-    new User('7507626699'), //saraf
-    new User('9168875988')  //jain
-]
-
-somuCoin.addUsers(voters)
-
-candidates = [
-    new User('9663644558'), //Hriday G
-    new User('9039051015')  //Anand
-]
-
-somuCoin.addUsers(candidates)
+let voteCoin = new blockchain()
 
 app.listen(PORT , () => {
     console.log(`App listening at port ${PORT}`)
 })
 
-app.get('/' , (req , res) => {
-    res.send('Hello World')
+app.route('/')
+.get((req , res) => {
+    res.sendFile('./public/index.html' , {
+        root : __dirname
+    });    
+})
+.post(async (req , res) => {
+    createUser(req.body.phoneNumberofUser).then((data) => {
+        if(data['newUser']){
+            voteCoin.addUser(data['user'])
+            console.log('New User Added')
+            res.redirect('/verifyPhone/' + req.body.phoneNumberofUser)
+        }
+        else{
+            console.log('Existing user found')
+            res.redirect('/dashboard' + req.body.phoneNumberofUser)
+        }
+    })
 })
 
+app.route('/verifyPhone/:phone_no')
+.get((req , res) => {
+    res.render('auth' , {
+        action_path : '/verifyPhone/' + req.params.phone_no
+    })
+})
+.post(async (req , res) => {
+    console.log(req.body)
+    res.render('privatekey' , {
+        action_path : '/dashboard/' + req.params.phone_no
+    })
+})
+
+app.route('/dashboard/:phone_no')
+.get((req , res) => {
+    res.render('dashboard' , {
+        action_path : '/dashboard/' + req.params.phone_no
+    })
+})
+.post((req , res) => {
+    console.log(req.body.dept)
+    
+    res.redirect('/confirmKey/' + req.params.phone_no + '/pub_key_of_Prakhar')
+})
+
+app.route('/confirmKey/:phone_no/:pub_key')
+.get((req , res) => {
+    res.render('confirmkey' , {
+        action_path : '/confirmKey/' + req.params.phone_no + '/' + req.params.pub_key
+    })
+})
+.post((req , res) => {
+    if(req.body.butt == 'verify'){
+        
+        res.sendFile('./public/thankyou.html' , {
+            root : __dirname
+        })   
+    }
+    else if(req.body.butt == 'return'){
+        res.redirect('/dashboard/' + req.params.phone_no)
+    }
+})
 
 
 // tx_list = [
--- src/user.js
+++ src/user.js
@@ -5,43 +5,86 @@ const EC = require('elliptic').ec
 const ec = new EC('secp256k1')
 
 const URL = 'mongodb://localhost:27017'
-const dbName = 'BITS-Voting'
+const dbName = 'crypto'
 
 const _userSchema = new mongoose.Schema({
     phone_no : Number,
     pub_key : String
 })
-const _user = mongoose.model('_user' , _userSchema)
+const userModel = mongoose.model('user' , _userSchema)
 
 
 
 class User{
-    constructor(_phone){
-        this.phone_np = _phone
 
-        mongoose.connect(URL + '/' + dbName)
+    constructor(_phone , _pub_key){
+        
+        this.phone_no = _phone
+        this.pub_key = _pub_key
+    }
+    getPubKey(){
+        return this.pub_key
+    }
 
-        const key = ec.genKeyPair()
+    signTransaction(tx){
+        if(this.getPubKey() !== tx.fromAddr)
+            throw new Error('You cannot sign transactions for other wallets')
+        const hashTx = tx.calculateHash()
+        const sig = key.sign(hashTx , 'base64')
+        tx.signature = sig.toDER('hex')
+    }
 
-        this.getPubKey = () => {
-            return key.getPublic('hex')
-        }
-
-        this.signTransaction = (tx) => {
-            if(this.getPubKey() !== tx.fromAddr)
-                throw new Error('You cannot sign transactions for other wallets')
-            const hashTx = tx.calculateHash()
-            const sig = key.sign(hashTx , 'base64')
-            tx.signature = sig.toDER('hex')
-        }
-
-        this.sendMoney = (user , amt) => {
-            const tx = new transaction(this.getPubKey() , user.getPubKey() , amt)
-            this.signTransaction(tx)
-            return tx
-        }
-
+    sendMoney(user , amt){
+        const tx = new transaction(this.getPubKey() , user.getPubKey() , amt)
+        this.signTransaction(tx)
+        return tx
     }
 }
 
+async function createUser(_phone){
+
+    return new Promise ((resolve , reject) => {
+
+        mongoose.connect(URL + '/' + dbName)
+
+        const res = {
+            'newUser' : false,
+            'pr_key' : null
+        }
+
+        userModel.find({phone_no : _phone} , {} , (err , users) => {
+            if(err)
+                reject
+            else{
+                if(users.length == 0){
+                    const key = ec.genKeyPair()
+                    pub_key = key.getPublic('hex')
+                    const _user = new userModel({
+                        phone_no : _phone,
+                        pub_key : key.getPublic('hex')
+                    })
+                    _user.save((err , saved_user) => {
+                        if(err)
+                            console.log(err)
+                        else
+                            console.log('User Saved')
+                    })
+                    res['newUser'] = true
+                    res['pr_key'] = key.getPrivate('hex')
+                }
+                else{
+                    pub_key = users[0]['pub_key']
+                    console.log('User already present')
+                }
+    
+                const user = new User(_phone , pub_key)
+                res['user'] = user
+                resolve(res)
+            }
+        })
+
+    })
+}
+
 module.exports.User = User
+module.exports.createUser = createUser
